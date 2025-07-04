cb5f151d72e7a99d2b24b9ca38cafd124428b09f
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-07-04 11:01:49 -0700

    Update BrowsePets.jsx (#25)

--- frontend/src/pages/BrowsePets.jsx
+++ frontend/src/pages/BrowsePets.jsx
@@ -1,29 +1,27 @@
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import BrowsePetsHeading from '../components/BrowsePetsHeading';
-import PetCards from '../components/PetCards';
-import PetFilter from '../components/PetFilter';
-
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import BrowsePetsHeading from "../components/BrowsePetsHeading";
+import PetGrid from "../components/PetGrid";
+import PetFilter from "../components/PetFilter";
 
 function BrowsePets(user) {
-
-    console.log("Welcome ", user); // You can use whoever is logged in to manipulate the pages (if someone is logged in) -> Cards / Favorites in this case!
+  console.log("Welcome ", user); // You can use whoever is logged in to manipulate the pages (if someone is logged in) -> Cards / Favorites in this case!
 
   return (
     <>
       <div>
         <Header />
         <Navbar />
-        <BrowsePetsHeading/>
+        <BrowsePetsHeading />
         <section id="pet-gallery">
-            <PetFilter/>
-            <PetCards/>
+          <PetFilter />
+          <PetGrid />
         </section>
         <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default BrowsePets
+export default BrowsePets;
