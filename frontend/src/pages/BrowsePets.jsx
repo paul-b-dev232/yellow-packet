7f3977ed13bae1de936f45c7dfb2a6a8312cd272
UhOhGio <gioehrig@gmail.com>
2025-07-09 22:34:36 -0700

    Implemented a Pet Filter system

--- frontend/src/pages/BrowsePets.jsx
+++ frontend/src/pages/BrowsePets.jsx
@@ -4,18 +4,27 @@ import Footer from "../components/Footer";
 import BrowsePetsHeading from "../components/BrowsePetsHeading";
 import PetGrid from "../components/PetGrid";
 import PetFilter from "../components/PetFilter";
+import { useState } from "react";
 
-function BrowsePets(user) {
-  console.log("Welcome ", user); // You can use whoever is logged in to manipulate the pages (if someone is logged in) -> Cards / Favorites in this case!
+function BrowsePets({ user }) {
+  const [filters, setfilters] = useState({ type: '', shelter: '', age_stage: '', size: '', sex: ''}); // holds the value of each applied filter, for use with PetGrid.
+
+  // Values for testing: Swap these with values from the database
+  const types = ["Dog", "Cat"];
+  const shelters = ["Colorado Shelter", "Cali Shelter"];
+  const age_stages = ["Youth", "Adult", "Senior"];
+  const sizes = ["Small", "Medium", "Large"];
+  const sexes = ["Male", "Female"];
 
   return (
     <>
       <div>
-        <Header />
+        <Header user={user}/>
         <Navbar />
         <BrowsePetsHeading />
         <section id="pet-gallery">
-          <PetFilter />
+          <PetFilter filters={filters} setfilters={setfilters} types={types} shelters={shelters} age_stages={age_stages} sizes={sizes} sexes= {sexes}/>
+          <div>Testing: {JSON.stringify(filters)}</div> {/* remove for production */}
           <PetGrid />
         </section>
         <Footer />
